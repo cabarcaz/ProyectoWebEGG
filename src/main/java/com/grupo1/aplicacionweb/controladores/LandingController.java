f5101f6715ab8ad6d19a59462d064bc56421388e
Camilo Abarca <cabarcaz@yahoo.cl>
2022-02-10 15:34:47 -0300

    Se modifica controlador del landing, no era el index.

--- src/main/java/com/grupo1/aplicacionweb/controladores/LandingController.java
+++ src/main/java/com/grupo1/aplicacionweb/controladores/LandingController.java
@@ -1,16 +1,17 @@
 package com.grupo1.aplicacionweb.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping({"","/"})
+@RequestMapping("/landing")
 public class LandingController {
 
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String landing(Model model){
+        model.addAttribute("titulo", "Inicio");
+        return "/main/landing";
     }
 }
