a9294bf3884c8370f4cd6e7650bf653444cc2c92
Camilo Abarca <cabarcaz@yahoo.cl>
2022-02-13 20:41:49 -0300

    se avanza con ingreso de la receta

--- src/main/java/com/grupo1/aplicacionweb/controladores/RecetaController.java
+++ src/main/java/com/grupo1/aplicacionweb/controladores/RecetaController.java
@@ -3,19 +3,21 @@ package com.grupo1.aplicacionweb.controladores;
 import com.grupo1.aplicacionweb.entidades.Receta;
 import com.grupo1.aplicacionweb.servicio.RecetaServicio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-
-
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.List;
 
 import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 @RequestMapping("/receta")
@@ -28,22 +30,37 @@ public class RecetaController {
         List<Receta> listaRecetas = recetaServicio.listar();
         model.addAttribute("recetas",listaRecetas);
         model.addAttribute("titulo","Listadi Recetas");
-        model.addAttribute("h1","Formulario ingreso de nueva receta");
+        model.addAttribute("h1","Lista de Recetas Existentes");
         return "/receta/lista";
     }
 
     @GetMapping("/crear")
     public String crearReceta(Model model) {
         Receta receta = new Receta();
-        model.addAttribute("receta",receta);
+        model.addAttribute("recetas",receta);
         model.addAttribute("titulo","Formulario");
         model.addAttribute("h1","Formulario ingreso de recetas");
         return "/receta/nuevo";
     }
 
     @PostMapping("/guardar")
-    public String guardar(@Valid @ModelAttribute Receta receta,Model model) {
+    public String guardar(@Valid @ModelAttribute Receta receta,Model model, SessionStatus ss,@RequestParam("file") MultipartFile imagen) {
+
+        if(!imagen.isEmpty()) {
+            Path directorioImagenes = Paths.get("src//main//resources//static//imagenes/receta");
+            String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath();
+            try {
+                byte[] bytesImg = imagen.getBytes();
+                Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename());
+                Files.write(rutaCompleta, bytesImg);
+                receta.setFoto(imagen.getOriginalFilename());
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
         recetaServicio.crear(receta);
+        ss.setComplete();
         return "redirect:/receta/";
     }
 
--- src/main/java/com/grupo1/aplicacionweb/entidades/Receta.java
+++ src/main/java/com/grupo1/aplicacionweb/entidades/Receta.java
@@ -1,7 +1,6 @@
 package com.grupo1.aplicacionweb.entidades;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.util.List;
 
@@ -15,11 +14,11 @@ public class Receta implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotEmpty(message = "Este campo es obligatorio.")
+    // @NotEmpty(message = "Este campo es obligatorio.")
     @Column(name = "nombre")
     private String nombre;
 
-    @NotEmpty(message = "Este campo es obligatorio.")
+    // @NotEmpty(message = "Este campo es obligatorio.")
     @Column(name = "preparacion")
     private String preparacion; // considerar armar arraylist para iterar en front
 
@@ -31,19 +30,19 @@ public class Receta implements Serializable {
     @Column(name = "comentario")
     private String comentario;
 
-    @NotEmpty(message = "Este campo es obligatorio.")
+    // @NotEmpty(message = "Este campo es obligatorio.")
     @Column(name = "tiempo_coccion")
     private String tiempoDeCoccion;
 
-    @NotEmpty(message = "Este campo es obligatorio.")
+    // @NotEmpty(message = "Este campo es obligatorio.")
     @Column(name = "tiempo_preparacion")
     private String tiempoDePreparacion;
 
-    @NotEmpty(message = "Este campo es obligatorio.")
+    // @NotEmpty(message = "Este campo es obligatorio.")
     @Column(name = "tiempo_total")
     private String tiempoTotal;
 
-    @NotEmpty (message = "Este campo es obligatorio.")
+    // @NotEmpty (message = "Este campo es obligatorio.")
     @Column(name = "procion")
     private String porcion;
 
