94a74ea229cefeb2c194e3b62bce4fa4c61c81be
Camilo Abarca <cabarcaz@yahoo.cl>
2022-02-14 19:11:42 -0300

    reloj

--- src/main/java/com/grupo1/aplicacionweb/controladores/RecetaController.java
+++ src/main/java/com/grupo1/aplicacionweb/controladores/RecetaController.java
@@ -16,11 +16,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 @RequestMapping("/receta")
+@SessionAttributes("receta")
 public class RecetaController {
     @Autowired
     private RecetaServicio recetaServicio;
@@ -29,7 +29,7 @@ public class RecetaController {
     public String listar(Model model) {
         List<Receta> listaRecetas = recetaServicio.listar();
         model.addAttribute("recetas",listaRecetas);
-        model.addAttribute("titulo","Listadi Recetas");
+        model.addAttribute("titulo","Listado de  Recetas");
         model.addAttribute("h1","Lista de Recetas Existentes");
         return "/receta/lista";
     }
@@ -37,14 +37,14 @@ public class RecetaController {
     @GetMapping("/crear")
     public String crearReceta(Model model) {
         Receta receta = new Receta();
-        model.addAttribute("recetas",receta);
         model.addAttribute("titulo","Formulario");
         model.addAttribute("h1","Formulario ingreso de recetas");
+        model.addAttribute("recetas",receta);
         return "/receta/nuevo";
     }
 
     @PostMapping("/guardar")
-    public String guardar(@Valid @ModelAttribute Receta receta,Model model, SessionStatus ss,@RequestParam("file") MultipartFile imagen) {
+    public String guardar(@Valid @ModelAttribute Receta receta, SessionStatus ss,Model model,@RequestParam("file") MultipartFile imagen) {
 
         if(!imagen.isEmpty()) {
             Path directorioImagenes = Paths.get("src//main//resources//static//imagenes/receta");
