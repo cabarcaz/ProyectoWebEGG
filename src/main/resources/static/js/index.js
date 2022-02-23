6d4aaec95d9542feb661bd7a6652dd0e05348d4f
Camilo Abarca <cabarcaz@yahoo.cl>
2022-02-23 10:25:21 -0300

    se agrega funcion de subir con boton.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -51,3 +51,27 @@ const formatoHora = (hora) => {
 
 setInterval(mostrarReloj, 1000);
 
+
+
+//boton up
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
+
+
+
+
