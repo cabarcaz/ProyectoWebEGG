d98eea11b1e3afa8e5213258284c4ba87ca614c6
Camilo Abarca <cabarcaz@yahoo.cl>
2022-03-08 23:52:06 -0300

    Se modifica el redireccionamiento del boton salir.

--- src/main/java/com/grupo1/aplicacionweb/controladores/IndexController.java
+++ src/main/java/com/grupo1/aplicacionweb/controladores/IndexController.java
@@ -5,25 +5,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/index")
 public class IndexController {
 
     @GetMapping("/")
-    public String loginIndex(Model model, @RequestParam (required = false ) String error,
-                             @RequestParam (required = false) String email,
-                             @RequestParam (required = false) String logout) {
+    public String loginIndex(Model model, @RequestParam(required = false) String error,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String logout,
+            RedirectAttributes attribute){
         if (error != null) {
             model.addAttribute("error", "el usuario ingresado o la contraseña son incorrectos");
         }
-        if (email != null){
-            model.addAttribute("email",email);
+        if (email != null) {
+            model.addAttribute("email", email);
+        }
+        if (logout != null) {
+            attribute.addFlashAttribute("success", "SESION FINALIZADA CON EXITO.");
+            model.addAttribute("success", "SESION FINALIZADA CON EXITO.");
         }
         model.addAttribute("titulo", "Inicio");
 
         return "/index";
     }
-
 
 }
